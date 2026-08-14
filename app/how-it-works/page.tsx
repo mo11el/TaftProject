663e7c1e55b8d378582d19555b6db0e33075f9d8
mo11el <ianmollel1@gmail.com>
2026-08-14 15:55:33 -0400

    Add how-it-works page that embeds new-how-it-works_1.html

---
+++ app/how-it-works/page.tsx
@@ -0,0 +1,8 @@
+export default function HowItWorksPage() {
+  return (
+    <iframe
+      src="/new-how-it-works_1.html"
+      style={{ width: '100vw', height: '100vh', border: 'none', display: 'block' }}
+    />
+  )
+}
